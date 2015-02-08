d0e43706a2d8d2b3731f35630e12db433f2f519c
berkas1 <berka@berkasimon.com>
2015-02-08 22:49:12 +0100

    GA code fix 2

--- src/template/template.php
+++ src/template/template.php
@@ -29,7 +29,7 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', <?php echo($meeting['ga_code']);?>, 'auto');
+    ga('create', '<?php echo($meeting['ga_code']);?>', 'auto');
     ga('send', 'pageview');
 
   </script>
